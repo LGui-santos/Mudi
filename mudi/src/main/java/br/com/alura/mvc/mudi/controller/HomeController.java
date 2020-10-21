804a6b7fa581cbb1aaca3754129b02b75b2a77fd
LGui-santos <guilhermesantos.srs@gmail.com>
2020-10-21 00:11:13 -0300

    Fazendo conexão com o banco de dados, e criando uma classe do´tipo repository

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,29 +1,26 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
-
+	
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi redmi note 8");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/51Fv-PIiDQL.__AC_SY300_QL70_ML2_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-4RAM/dp/B07Y9XWK4M?source=ps-sl-shoppingads-lpcontext&psc=1");
-		pedido.setDescricao("Smartphone Xiaomi Redmi note 8 64GB 4GB RAM Azul Versão Global\r\n" + 
-				"Sistema operacional: MIUI 10 (Base no sistema operacional Android 9)\r\n" + 
-				"RAM: 4GB ROM: 64GB\r\n" + 
-				"Tela 6.3");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = pedidoRepository.readAll();
+
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
