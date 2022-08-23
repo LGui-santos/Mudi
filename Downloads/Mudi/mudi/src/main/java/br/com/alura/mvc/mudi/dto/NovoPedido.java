483aa2b6e7328ef615521b4354092864276eeefe
Guilherme <guilhermesantos.srs@gmail.com>
2022-08-23 01:42:47 -0300

    implementando autenticação com spring security

---
+++ Downloads/Mudi/mudi/src/main/java/br/com/alura/mvc/mudi/dto/NovoPedido.java
@@ -0,0 +1,60 @@
+package br.com.alura.mvc.mudi.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+
+public class NovoPedido {
+
+	@NotBlank
+	private String nomeProduto;
+	@NotBlank
+	private String urlProduto;
+	@NotBlank
+	private String imagemProduto;
+	private String descricaoProduto;
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+
+	public String getImagemProduto() {
+		return imagemProduto;
+	}
+
+	public void setImagemProduto(String imagemProduto) {
+		this.imagemProduto = imagemProduto;
+	}
+
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setUrlProduto(urlProduto);
+		pedido.setUrlImagem(imagemProduto);
+		pedido.setDescricao(descricaoProduto);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		return pedido;
+	}
+
+}
